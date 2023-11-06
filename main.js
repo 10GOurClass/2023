7cdacc04507e42fedb661526f333fca780159d4b
10GOurClass <150004050+10GOurClass@users.noreply.github.com>
2023-11-07 00:06:46 +0500

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const cardsStyle = document.getElementsByClassName("cards")
             setTimeout(function(){
                 for (var i = 0; i < cardsStyle.length; i++) {
                     cardsStyle[i].style.width = "150px"
+                    cardsStyle[i].style.height = "150px"
                     cardsStyle[i].style.boxShadow = "0px 0px 0px rgb(180, 139, 139)"
                 }
             } , 400)
@@ -23,29 +24,54 @@ function toggleCard(cardId) {
     var name = document.getElementsByClassName("name")[cardId-1];
     
     if (openCardId !== null && openCardId !== cardId) {
-        var openCard = document.getElementById("card" + openCardId);
-        var openDescription = document.getElementById("description" + openCardId);
-        var openName = document.getElementsByClassName("name")[openCardId-1];
-        
-        openCard.style.width = "150px";
-        openCard.style.height = "150px";
-        openDescription.style.display = "none";
-        openName.style.display = "block";
-    }
-    
-    if (card.style.width === "300px" && card.style.height === "300px") {
-        card.style.width = "150px";
-        card.style.height = "150px";
-        description.style.display = "none";
-        name.style.display = "block";
-        openCardId = null;
-    } else {
-        card.style.width = "300px";
-        card.style.height = "300px";
-        description.style.display = "block";
-        name.style.display = "none";
-        openCardId = cardId;
-    }
+      var openCard = document.getElementById("card" + openCardId);
+      var openDescription = document.getElementById("description" + openCardId);
+      var openName = document.getElementsByClassName("name")[openCardId-1];
+      
+      openCard.style.width = "150px";
+      openCard.style.height = "150px";
+      openCard.style.filter = "blur(0px)";
+      openDescription.style.display = "none";
+      openName.style.display = "block"; 
+
+      var cards = document.getElementsByClassName("cards");
+      for (var i = 0; i < cards.length; i++) {
+          if (cards[i].id !== "card" + cardId) {
+              cards[i].style.opacity = "0.5";
+          }
+      }
+  }
+  
+  var cards = document.getElementsByClassName("cards");
+  for (var i = 0; i < cards.length; i++) {
+      if (cards[i].id !== "card" + cardId) {
+          cards[i].style.opacity = "1";
+          cards[i].style.filter = "none"
+      }
+  }
+  
+  if (card.style.width === "300px" && card.style.height === "300px") {
+      card.style.width = "150px";
+      card.style.height = "150px";
+      description.style.display = "none";
+      name.style.display = "block";
+      openCardId = null;     
+  } else {
+      card.style.width = "300px";
+      card.style.height = "300px";
+      description.style.display = "block";
+      name.style.display = "none";
+      openCardId = cardId;
+
+      var cards = document.getElementsByClassName("cards");
+      for (var i = 0; i < cards.length; i++) {
+          if (cards[i].id !== "card" + cardId) {
+              cards[i].style.opacity = "0.5";
+              cards[i].style.filter = "blur(5px)"
+          }
+      }
+      
+  }
 }
 for (var i = 1; i < 21; i++) {
     description = document.getElementById("description" + i);
